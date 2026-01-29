90bd7388c9a5d0a5f4c59b4922477892fba09532
Dogxi <hi@dogxi.me>
2026-01-30 01:20:23 +0800

    chore(web): Rebuild

--- web/.vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ web/.vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BzQOFbH6.js","_app/immutable/chunks/DJZ9pY1Y.js","_app/immutable/chunks/BfU-SR_P.js","_app/immutable/chunks/cijIjJqi.js","_app/immutable/chunks/BlrRteru.js","_app/immutable/chunks/BzzkJGNb.js"];
+export const imports = ["_app/immutable/nodes/1.DaSd8t75.js","_app/immutable/chunks/DJZ9pY1Y.js","_app/immutable/chunks/BfU-SR_P.js","_app/immutable/chunks/cijIjJqi.js","_app/immutable/chunks/Dl2hJ7pb.js","_app/immutable/chunks/BzzkJGNb.js"];
 export const stylesheets = [];
 export const fonts = [];
--- web/.vercel/output/functions/![-]/catchall.func/.svelte-kit/vercel-tmp/manifest.js
+++ web/.vercel/output/functions/![-]/catchall.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.svg"]),
 	mimeTypes: {".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.DbsIZn0Z.js",app:"_app/immutable/entry/app.Y5E83MvU.js",imports:["_app/immutable/entry/start.DbsIZn0Z.js","_app/immutable/chunks/BlrRteru.js","_app/immutable/chunks/BfU-SR_P.js","_app/immutable/chunks/BzzkJGNb.js","_app/immutable/entry/app.Y5E83MvU.js","_app/immutable/chunks/BfU-SR_P.js","_app/immutable/chunks/cijIjJqi.js","_app/immutable/chunks/DJZ9pY1Y.js","_app/immutable/chunks/BzzkJGNb.js","_app/immutable/chunks/ByKlfcM1.js","_app/immutable/chunks/vy3UjScA.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.D00ep0T8.js",app:"_app/immutable/entry/app.BQlAR1Um.js",imports:["_app/immutable/entry/start.D00ep0T8.js","_app/immutable/chunks/Dl2hJ7pb.js","_app/immutable/chunks/BfU-SR_P.js","_app/immutable/chunks/BzzkJGNb.js","_app/immutable/entry/app.BQlAR1Um.js","_app/immutable/chunks/BfU-SR_P.js","_app/immutable/chunks/cijIjJqi.js","_app/immutable/chunks/DJZ9pY1Y.js","_app/immutable/chunks/BzzkJGNb.js","_app/immutable/chunks/ByKlfcM1.js","_app/immutable/chunks/vy3UjScA.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
